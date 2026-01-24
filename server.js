8b675c72a5775d883e5490b240a107b3a25e0cbb
shubham <shubhamnarayan45@gmail.com>
2026-01-24 14:38:39 +0530

    comment

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 8000;
 
+//origins changed
 const allowedOrigins = [
   "http://localhost:5173",
   "https://demoschool.aadishrisoftech.com",
